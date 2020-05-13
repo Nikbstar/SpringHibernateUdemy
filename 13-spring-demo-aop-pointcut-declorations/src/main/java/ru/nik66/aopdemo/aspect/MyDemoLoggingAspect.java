03d7a2a0233ff68f6f498eff8e8801e8884577a1
Nikolay Kotkin <nikbstar@gmail.com>
2020-05-13 14:56:27 +0500

    332. AOP: Combining Pointcuts - Overview

--- 13-spring-demo-aop-pointcut-declorations/src/main/java/ru/nik66/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 13-spring-demo-aop-pointcut-declorations/src/main/java/ru/nik66/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,20 +2,24 @@ package ru.nik66.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* ru.nik66.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* ru.nik66.aopdemo.dao.*.*(..))")
+    public void doForDAOPackage() {}
+
+    @Before("doForDAOPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("===> Executing @Before advice on addAccount()");
     }
 
-    @Before("execution(* ru.nik66.aopdemo.dao.*.*(..))")
-    public void anotherBefore() {
-        System.out.println("===> This is another @Before advice");
+    @Before("doForDAOPackage()")
+    public void performApiAnalytics() {
+        System.out.println("===> Performing API analitycs");
     }
 
 }

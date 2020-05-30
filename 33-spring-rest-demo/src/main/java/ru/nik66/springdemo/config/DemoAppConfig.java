edd183c1763c642c3eecc970ff586c2922840692
Nikolay Kotkin <nikbstar@gmail.com>
2020-05-30 22:09:02 +0500

    462. Spring REST - App Enhancement with Default Page

---
+++ 33-spring-rest-demo/src/main/java/ru/nik66/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package ru.nik66.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.nik66.springdemo")
+public class DemoAppConfig {
+}
---
+++ 33-spring-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring REST Demo</title>
+</head>
+<body>
+    Spring REST Demo
+<hr />
+<a href="${pageContext.request.contextPath}/test/hello">REST Hello world!</a>
+</body>
+</html>
